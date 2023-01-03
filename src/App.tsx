23b3f39629751db4222bdadc723ff9e7ba8c63cc
biellopes00 <gabriellopes031997@hotmail.com>
2023-01-02 22:49:52 -0300

    projeto pomodoro

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import {BrowserRouter} from 'react-router-dom'
+import { ThemeProvider } from "styled-components"
+import { Router } from "./Router"
+
+import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/global"
+import { CyclesContextProvider } from './contexts/CyclesContext'
+
+export function App() {
+ 
+  return (
+   <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
+      <BrowserRouter>
+       <CyclesContextProvider>
+        <Router/>
+      </CyclesContextProvider>
+      </BrowserRouter>
+   </ThemeProvider>
+   
+  )
+}
+
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { HeaderContainer } from "./styles";
+import { Scroll, Timer } from "phosphor-react"
+
+import logoIgnite from '../../assets/logo.svg';
+import { NavLink } from "react-router-dom";
+
+export function Header() {
+    return(
+        <HeaderContainer>
+           <img src={logoIgnite} alt="" />
+            <nav>
+                <NavLink to="/">
+                    <Timer size={24}/>
+                </NavLink>
+                <NavLink to="/History">
+                    <Scroll size={24}/>
+                </NavLink>
+            </nav>
+        </HeaderContainer>
+    )
+}
